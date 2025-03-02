c429ddf4ad396dfdc9ed30c0eddd6f6fae4223e7
unknown <kotalaadam@gmail.com>
2025-03-02 15:25:06 +0100

    fix

---
+++ includes/licensing.php
@@ -0,0 +1,9 @@
+<?php
+if (!defined('ABSPATH')) {
+    exit; // Zabránění přímému přístupu
+}
+
+// Funkce pro správu licencí
+function bf_ecomail_check_license() {
+    return true; // Zatím pouze testovací verze
+}
